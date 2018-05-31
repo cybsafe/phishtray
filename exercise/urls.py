5871d550b6b61b063485866ce0194b4e0d1d93c6
ramesh <ramesh.sukka@telegraph.co.uk>
2018-05-31 12:08:38 +0100

    Enabled Exercise Model as Rest endpoint.

--- exercise/urls.py
+++ exercise/urls.py
@@ -2,11 +2,21 @@ from django.urls import path
 
 from . import views
 
+from .views import ExerciseViewSet
+
+from rest_framework import routers
+from django.conf.urls import url, include
+
+
 app_name = 'exercise'
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'exercise', ExerciseViewSet)
 
 urlpatterns = [
     path('<slug:link>/profile', views.profile, name='profile'),
     path('<slug:link>/start/<int:p_id>/', views.start, name='start'),
     path('<slug:link>/', views.index, name='index'),
-    path('', views.index, name='index'),
+    url(r'^', include(router.urls)),
 ]
