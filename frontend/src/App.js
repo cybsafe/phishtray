f483f2b51d29f574652a3efa73be5cb99d6b049c
Jonathan Kim <jkimbo@gmail.com>
2018-06-24 11:12:53 +0100

    Add emotion and style sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,74 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import { BrowserRouter, Route, Redirect, Link } from 'react-router-dom';
-import { Button } from 'carbon-components-react';
-import './App.css';
+import styled, { css } from 'react-emotion';
 
 import Inbox from './pages/Inbox';
 
-class App extends Component {
+const Container = styled('div')({
+  display: 'flex',
+  flexDirection: 'row',
+  height: '100vh',
+});
+
+const Sidebar = styled('div')({
+  flex: 0,
+  height: '100%',
+  background: '#161415',
+});
+
+const StyledLink = styled(Link)(
+  {
+    display: 'block',
+    padding: '20px 40px',
+    textDecoration: 'none',
+    letterSpacing: '1.5px',
+    fontSize: 18,
+  },
+  props => ({
+    fontWeight: props.isMatch ? 'bold' : 'normal',
+    color: props.isMatch ? '#FFF' : '#C1C1C1',
+  })
+);
+
+function SidebarLink({ to, children }) {
+  return (
+    <Route path={to}>
+      {({ match }) => (
+        <StyledLink to={to} isMatch={!!match}>
+          {children}
+        </StyledLink>
+      )}
+    </Route>
+  );
+}
+
+class App extends PureComponent {
   render() {
     return (
       <BrowserRouter>
-        <div>
-          <ul>
-            <li>
-              <Link to="/inbox">Inbox</Link>
-            </li>
-            <li>
-              <Link to="/accounts">Accounts</Link>
-            </li>
-            <li>
-              <Link to="/contacts">Contacts</Link>
-            </li>
-            <li>
-              <Link to="/web">Web</Link>
-            </li>
-            <li>
-              <Link to="/files">Files</Link>
-            </li>
-          </ul>
+        <Container>
+          <Sidebar>
+            <div
+              className={css({
+                height: 80,
+                width: '100%',
+                backgroundColor: '#1D1B1C',
+              })}
+            />
+            <SidebarLink className={css({ marginTop: 30 })} to="/inbox">
+              Inbox
+            </SidebarLink>
+            <SidebarLink to="/accounts">Accounts</SidebarLink>
+            <SidebarLink to="/contacts">Contacts</SidebarLink>
+            <SidebarLink to="/web">Web</SidebarLink>
+            <SidebarLink to="/files">Files</SidebarLink>
+          </Sidebar>
 
-          <hr />
-
-          <Redirect from="/" to="/inbox" />
-          <Route path="/inbox" component={Inbox} />
-        </div>
+          <div className={css({ flex: 1 })}>
+            <Redirect from="/" to="/inbox" />
+            <Route path="/inbox" component={Inbox} />
+          </div>
+        </Container>
       </BrowserRouter>
     );
   }
