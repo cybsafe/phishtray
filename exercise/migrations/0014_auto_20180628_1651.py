a932446f746dd86ddaf3e6e00cfc92d5a3175aa1
ramesh <ramesh.sukka@telegraph.co.uk>
2018-07-02 05:19:39 +0100

    fix build error.

--- exercise/migrations/0014_auto_20180628_1651.py
+++ exercise/migrations/0014_auto_20180628_1651.py
@@ -23,6 +23,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='exerciseemail',
             name='belongs_to',
-            field=models.ForeignKey(null=True, blank=True, on_delete=django.db.models.deletion.CASCADE, to='exercise.ExerciseEmail'),
+            field=models.ForeignKey(null=True, blank=True, on_delete=models.deletion.CASCADE, to='exercise.ExerciseEmail'),
         ),
     ]
