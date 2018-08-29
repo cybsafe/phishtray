41f2a9ab436828f206a2e96fb76bbba75360461f
Yohan Lebret <yohan.lebret@gmail.com>
2018-08-29 12:01:21 +0100

    Add view to store action in db

--- exercise/views.py
+++ exercise/views.py
@@ -1,13 +1,60 @@
+import json
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+
 from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.urls import reverse
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
 
-from exercise.models import Exercise, ExerciseEmail, ExerciseEmailReply
+from exercise.models import Exercise, ExerciseEmail, ExerciseEmailReply, ExerciseAttachment, ExerciseAction, EXERCISE_ACTION_TYPES, EXERCISE_REPLY_ACTION, EXERCISE_ATTACHMENT_ACTION
 from participant.models import Participant, ParticipantProfile
 from utils import helpers
 from rest_framework import serializers, viewsets
 from exercise.serializer import *
+
+ACTION_TYPES = [action_type[1] for action_type in EXERCISE_ACTION_TYPES]
+
+ACTION_JSON_SCHEMA = {
+    'type': 'object',
+    'properties': {
+        'milliseconds': {
+            'type': 'number',
+            'minimum': 0,
+        },
+        'action': {
+            'type': 'object',
+            'properties': {
+                'type': {
+                    'type': 'string',
+                    'pattern': '^{0}$'.format('|'.join(ACTION_TYPES)),
+                },
+                'associations': {
+                    'type': 'object',
+                    'properties': {
+                        'exerciseEmail': {
+                            'type': 'number',
+                            'minimum': 0,
+                        },
+                        'exerciseEmailReply': {
+                            'type': 'number',
+                            'minimum': 0,
+                        },
+                        'exerciseAttachment': {
+                            'type': 'number',
+                            'minimum': 0,
+                        },
+                    },
+                    'required': ['exerciseEmail'],
+                },
+            },
+            'required': ['type', 'associations'],
+        },
+    },
+    'required': ['milliseconds', 'action'],
+}
 
 
 def index(request, link):
@@ -66,3 +113,50 @@ class ExerciseEmailThreadViewSet(viewsets.ModelViewSet):
     """
     queryset = ExerciseEmail.objects.all()
     serializer_class = EmailDetailsSerializer
+
+
+@csrf_exempt
+@require_http_methods(['POST'])
+def store_action(request):
+    body = request.body
+    content = json.loads(body)
+
+    # validate json format
+    try:
+        validate(content, ACTION_JSON_SCHEMA)
+    except ValidationError:
+        return JsonResponse({'message': 'Json validation error', 'status': False}, status=500)
+
+    try:
+        action_type = content['action']['type']
+        associations = content['action']['associations']
+        exercise_email_id = associations['exerciseEmail']
+
+        action = None
+        exercise_email_reply = None
+        exercise_attachment = None
+        exercise_email = ExerciseEmail.objects.get(id=exercise_email_id)
+
+        # get the reply or attachment instance corresponding to the action
+        if action_type == 'email_reply' and 'exerciseEmailReply' in associations:
+            exercise_email_reply = ExerciseEmailReply.objects.get(id=associations['exerciseEmailReply'])
+            action = EXERCISE_REPLY_ACTION
+        elif action_type == 'email_attachment_open' and 'exerciseAttachment' in associations:
+            exercise_attachment = ExerciseAttachment.objects.get(id=associations['exerciseAttachment'])
+            action = EXERCISE_ATTACHMENT_ACTION
+
+        exercise_action = ExerciseAction(
+            action = action,
+            email = exercise_email,
+            reply = exercise_email_reply,
+            attachment = exercise_attachment,
+        )
+        exercise_action.save()
+    except ExerciseEmail.DoesNotExist:
+        return JsonResponse({'message': 'Invalid email id', 'status': False}, status=500)
+    except ExerciseEmailReply.DoesNotExist:
+        return JsonResponse({'message': 'Invalid email reply id', 'status': False}, status=500)
+    except ExerciseAttachment.DoesNotExist:
+        return JsonResponse({'message': 'Invalid attachement id', 'status': False}, status=500)
+
+    return JsonResponse({'message': None, 'status': False}, status=200)
