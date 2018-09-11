dc90fc7b7adddf1a64b1a7aa0885bf8edb27d70c
Daniel Hodgkins <danielgeoffreyhodgkins@gmail.com>
2018-09-11 14:03:34 +0100

    data addition and form creation

--- frontend/src/data/exercises.js
+++ frontend/src/data/exercises.js
@@ -4,6 +4,23 @@ const EXERCISES = {
   time: '5 minutes',
   content:
     '# Vacuus et Athenae in fervoribus essent stamina \n## Videntur echidnae colorem sorores praestructa Liber cavus \n\nLorem markdownum animi, voce mihi aenae qui lumen hostem Hyleusque tangit. Haec\nortus conplectitur respondit nullo rapida foedere Lycisce castique, aret est\nNoctisque. **Pone suas** sic! Mihi ante vasta curvamine praesentem raptatur\nbarba sternere promissa in flexo.\n',
+  form: [
+    {
+      key: 432,
+      label: 'How old are you',
+      type: 'number',
+    },
+    {
+      key: 434,
+      label: 'How do you feel about privacy?',
+      type: 'string',
+    },
+    {
+      key: 435,
+      label: 'Job Role',
+      type: 'string',
+    },
+  ],
 };
 
 export function loadExercise() {
--- frontend/src/pages/Exercise/index.js
+++ frontend/src/pages/Exercise/index.js
@@ -1,7 +1,14 @@
 import React, { Component, Fragment } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import styled, { css } from 'react-emotion';
 import { connect } from 'react-redux';
-import { InlineLoading, Button } from 'carbon-components-react';
+import {
+  InlineLoading,
+  Button,
+  NumberInput,
+  TextInput,
+  Form,
+} from 'carbon-components-react';
 import ReactMarkdown from 'react-markdown';
 
 import {
@@ -18,13 +25,39 @@ const Container = styled('div')({
   marginTop: 0,
 });
 
+const WelcomeForm = ({ form, title }) => (
+  <Fragment>
+    {title}
+    <Form onSubmit={this.handleSubmit}>
+      {form.map(item => {
+        switch (item.type) {
+          case 'number':
+            return <NumberInput label={item.label} />;
+
+          case 'string':
+            return <TextInput labelText={item.label} />;
+        }
+      })}
+      <Button type="submit">Click here to start the E-tray</Button>
+    </Form>
+  </Fragment>
+);
+
+const Title = styled('div')({
+  display: 'block',
+});
+
 export class Exercise extends Component {
   async componentDidMount() {
     await this.props.loadExercises();
   }
 
+  nextPath(path) {
+    this.props.history.push(path);
+  }
+
   render() {
-    const { exercise, isLoaded } = this.props;
+    const { exercise, isLoaded, match } = this.props;
 
     if (!isLoaded) {
       return (
@@ -43,10 +76,30 @@ export class Exercise extends Component {
 
     return (
       <Fragment>
-        <Container>
-          <ReactMarkdown>{exercise.content}</ReactMarkdown>
-          <Button>Start</Button>
-        </Container>
+        <Switch>
+          <Route
+            exact
+            path={`${match.url}`}
+            render={() => (
+              <Container>
+                <Title>{exercise.exercise}</Title>
+                <ReactMarkdown>{exercise.content}</ReactMarkdown>
+                <Button onClick={() => this.nextPath('/welcome/form')}>
+                  Continue
+                </Button>
+              </Container>
+            )}
+          />
+          <Route
+            path={`${match.url}/form`}
+            render={() => (
+              <Container>
+                <Title>{exercise.exercise}</Title>
+                <WelcomeForm form={exercise.form} />
+              </Container>
+            )}
+          />
+        </Switch>
       </Fragment>
     );
   }
