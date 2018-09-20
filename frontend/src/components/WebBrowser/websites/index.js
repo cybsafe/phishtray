a80f314553032361c51ca79dbd9db2fff2591eb3
olu@azimuthltd.net <olu@azimuthltd.net>
2018-09-20 13:35:59 +0100

    add payment website

--- frontend/src/components/WebBrowser/websites/index.js
+++ frontend/src/components/WebBrowser/websites/index.js
@@ -1,9 +1,25 @@
 import MyOffice from './MyOffice';
+import MyPayment from './MyPayment';
 
 export default {
   myoffice: {
     isSecure: true,
     url: 'www.myoffice.com',
     component: MyOffice,
+  },
+  mypayment: {
+    isSecure: true,
+    url: 'www.moneytransfer.com',
+    component: MyPayment,
+  },
+  mypaymentFalse: {
+    isSecure: false,
+    url: 'www.moneytransfr.com',
+    component: MyPayment,
+  },
+  mypaymentFalseInsecure: {
+    isSecure: true,
+    url: 'www.moneytransfr.com',
+    component: MyPayment,
   },
 };
