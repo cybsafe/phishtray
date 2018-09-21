51395017ba8d8eb5a8c38b28d7248d54c2caeedb
Feri Csepregi <feri@31.digital>
2018-09-21 12:33:59 +0100

    GH-90 - add actions and profile entries to participant endpoint (list and detail)

--- participant/serializer.py
+++ participant/serializer.py
@@ -1,15 +1,48 @@
-from django.urls import reverse
 from rest_framework import serializers
-from rest_framework.decorators import action
 
 from .models import (
     Participant,
     ParticipantAction,
-    ParticipantProfileEntry
-)
+    ParticipantProfileEntry,
+    ActionLog)
+
+
+class ParticipantActionSerializer(serializers.ModelSerializer):
+    action_details = serializers.SerializerMethodField()
+
+    class Meta:
+        model = ParticipantAction
+        fields = ('id', 'action_details',)
+
+    def get_action_details(self, participant_action):
+        log_entries_queryset = ActionLog.objects.filter(action=participant_action)
+        data = {}
+
+        for entry in log_entries_queryset:
+            data[entry.name] = entry.value
+
+        return data
+
+
+class ParticipantProfileEntrySerializer(serializers.ModelSerializer):
+    question = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = ParticipantProfileEntry
+        fields = ('id', 'question', 'answer',)
+
+    def get_question(self, profile_entry):
+        return profile_entry.question
 
 
 class ParticipantSerializer(serializers.ModelSerializer):
+    actions = serializers.SerializerMethodField(read_only=True)
+    profile = ParticipantProfileEntrySerializer(read_only=True, many=True)
+
     class Meta:
         model = Participant
-        fields = ('id', 'exercise', 'profile', 'actions')
+        fields = ('id', 'exercise', 'profile', 'actions',)
+
+    def get_actions(self, participant):
+        participant_actions_queryset = ParticipantAction.objects.filter(participant=participant)
+        return ParticipantActionSerializer(participant_actions_queryset, many=True).data
