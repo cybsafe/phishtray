80f4e9872805625cf5cb41ba7c03f41b2ff2c540
Osman Ilyas <osmanilyas@hotmail.com>
2018-09-21 11:25:05 +0100

    remove redundant inbox reducer and state

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import exercise from './exerciseReducer';
-import inbox from './inboxReducer';
 import fileManager from './fileManagerReducer';
 import ui from './uiReducer';
 
 const rootReducer = combineReducers({
   exercise,
-  inbox,
   fileManager,
   ui,
 });
