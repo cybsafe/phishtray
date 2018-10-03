27f569c6e1cc13e8c68965f2c3f2b1e7d2a816ce
Olu Omoniyi <oluomoniyi@users.noreply.github.com>
2018-10-03 10:33:01 +0100

    update ui, move clearing down store logic (#106)

--- frontend/src/pages/Afterward/index.js
+++ frontend/src/pages/Afterward/index.js
@@ -1,13 +1,53 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Markdown from 'react-markdown';
+import styled from 'react-emotion';
+import { persistor } from '../../redux';
 
-const Afterward = () => {
-  return <div>Thanks for taking the exercise</div>;
+const Container = styled('div')({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: '100%',
+  backgroundColor: '#fff',
+  boxShadow: '0 1px 2px 0 rgba(0, 0, 0, 0.1)',
+  padding: '1rem',
+});
+
+const Title = styled('h1')({
+  display: 'block',
+  fontSize: ' 2.25rem',
+  lineHeight: 1.25,
+  marginBottom: '35px',
+  fontWeight: 300,
+});
+
+type Props = {
+  afterwardMessage: String,
 };
+
+class Afterward extends React.Component<Props> {
+  componentDidMount() {
+    localStorage.clear();
+    persistor.purge();
+    clearInterval();
+  }
+
+  render() {
+    return (
+      <Container>
+        <Title>Thanks for taking the exercise.</Title>
+        {this.props.afterwardMessage && (
+          <Markdown source={this.props.afterwardMessage} />
+        )}
+      </Container>
+    );
+  }
+}
 
 export default connect(
   state => ({
-    state: state,
+    afterwardMessage: state.exercise.afterward,
   }),
   {}
 )(Afterward);
