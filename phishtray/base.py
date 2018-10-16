2f4f5694e99dfb350e201a670b46307aef32e9f4
Feri Csepregi <feri@31.digital>
2018-10-16 14:47:17 +0100

    fixup! GH-125 - CSV serializer for Participant ActionLogs, exercise-reports view

--- phishtray/base.py
+++ phishtray/base.py
@@ -60,30 +60,3 @@ class PhishtrayBaseModel(SoftDeletionModel):
 
     class Meta:
         abstract = True
-
-
-class CSVResponseMixin(object):
-    csv_filename = 'csvfile.csv'
-
-    def get_csv_filename(self):
-        return self.csv_filename
-
-    def render_to_csv(self, data):
-        response = HttpResponse(content_type='text/csv')
-        cd = 'attachment; filename="{0}"'.format(self.get_csv_filename())
-        response['Content-Disposition'] = cd
-
-        writer = csv.writer(response)
-        for row in data:
-            writer.writerow(row)
-
-
-        # writer =
-
-        # with tempfile.TemporaryFile(mode='w') as fp:
-        #     writer = csv.DictWriter(fp, fieldnames=csv_headers)
-        #     writer.writeheader()
-        #     for row in csv_rows:
-        #         writer.writerow(row)
-
-        return response
