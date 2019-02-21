852e1062e13db826b281c44508a67fbaa558a43c
Feri <feri@31.digital>
2019-02-21 15:44:11 +0000

    NOTICKET - tidy up tests to improve on runtime (#189)

--- phishtray/test/tests.py
+++ phishtray/test/tests.py
@@ -8,16 +8,16 @@ class PhishtrayBaseModelTest(TestCase):
 
     def setUp(self):
         self.exercise_1 = ExerciseFactory()
-        ExerciseFactory.create_batch(10)
+        self.exercise_2 = ExerciseFactory()
 
     def test_soft_delete(self):
         # Check that we have all Exercises
-        self.assertEqual(11, Exercise.objects.all().count())
+        self.assertEqual(2, Exercise.objects.all().count())
 
         # Delete one then we should end up with 10 when accessing the DB with django
         self.exercise_1.delete()
         self.assertFalse(Exercise.objects.filter(pk=self.exercise_1.id))
-        self.assertEqual(10, Exercise.objects.all().count())
+        self.assertEqual(1, Exercise.objects.all().count())
 
         # However the record should still be present in the DB
         raw_exercises = Exercise.objects.raw('SELECT * FROM exercise_exercise')
@@ -26,26 +26,26 @@ class PhishtrayBaseModelTest(TestCase):
         deleted_exercises = [e for e in raw_exercises if e.deleted_at]
         undeleted_exercises = [e for e in raw_exercises if not e.deleted_at]
 
-        self.assertEqual(11, len(all_exercises))
+        self.assertEqual(2, len(all_exercises))
         self.assertEqual(1, len(deleted_exercises))
-        self.assertEqual(10, len(undeleted_exercises))
+        self.assertEqual(1, len(undeleted_exercises))
 
     def test_undo_soft_delete(self):
         # Delete one then we should end up with 10 when accessing the DB with django
         self.exercise_1.delete()
         self.assertFalse(Exercise.objects.filter(pk=self.exercise_1.id))
-        self.assertEqual(10, Exercise.objects.all().count())
+        self.assertEqual(1, Exercise.objects.all().count())
 
         exercise = Exercise.all_objects.get(pk=self.exercise_1.id)
         exercise.deleted_at = None
         exercise.save()
 
-        self.assertEqual(11, Exercise.objects.all().count())
+        self.assertEqual(2, Exercise.objects.all().count())
 
     def test_hard_delete(self):
         self.exercise_1.hard_delete()
         self.assertFalse(Exercise.objects.filter(pk=self.exercise_1.id))
-        self.assertEqual(10, Exercise.objects.all().count())
+        self.assertEqual(1, Exercise.objects.all().count())
 
         # Get all records from the DB
         raw_exercises = Exercise.objects.raw('SELECT * FROM exercise_exercise')
@@ -53,6 +53,6 @@ class PhishtrayBaseModelTest(TestCase):
         deleted_exercises = [e for e in raw_exercises if e.deleted_at]
         undeleted_exercises = [e for e in raw_exercises if not e.deleted_at]
 
-        self.assertEqual(10, len(all_exercises))
+        self.assertEqual(1, len(all_exercises))
         self.assertEqual(0, len(deleted_exercises))
-        self.assertEqual(10, len(undeleted_exercises))
+        self.assertEqual(1, len(undeleted_exercises))
