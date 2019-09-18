adee6b7b025beac6afd6b887017da0c940760c52
Asghar Sir <asgharsir@gmail.com>
2019-09-18 09:33:59 +0100

    208-Added decorator to register admin model

--- participant/admin.py
+++ participant/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from .models import Participant
 
 
+@admin.register(Participant)
 class ParticipantList(admin.ModelAdmin):
     list_filter = ("exercise",)
-
-
-admin.site.register(Participant, ParticipantList)
