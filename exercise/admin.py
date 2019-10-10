c888ce3b641da175092af9b60f7e3d172634f220
Marcelo Pacobahyba <marcelo@cybsafe.com>
2019-10-10 11:35:46 +0100

    Admin and Model Adjusts

--- exercise/admin.py
+++ exercise/admin.py
@@ -1,6 +1,7 @@
 from django import forms, template
 from django.contrib import admin
-from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import redirect
 from .models import (
     DemographicsInfo,
     Exercise,
@@ -24,20 +25,28 @@ class ExerciseAdminForm(forms.ModelForm):
 
 class ExerciseAdmin(admin.ModelAdmin):
     form = ExerciseAdminForm
-    list_display = ("id", "title", "description", "length_minutes")
+    list_display = ("id", "title", "description", "length_minutes", "created_date")
     readonly_fields = ("copied_from",)
     change_form_template = "admin/exercise/change_form.html"
+    ordering = ("-created_date",)
 
     def response_change(self, request, obj):
         if "_copy_exercise" in request.POST:
-            copied_from = Exercise.objects.get(pk=obj.id)
+            # Create a copy of the Exercise
+            original_exercise = Exercise.objects.get(pk=obj.id)
             obj.title = obj.title + " Copy"
-            obj.copied_from = copied_from
+            if not obj.copied_from:
+                obj.copied_from = original_exercise
             obj.id = None
             obj.save()
 
+            # Add ManyToMany connections
+            obj.demographics.add(*original_exercise.demographics.all())
+            obj.emails.add(*original_exercise.emails.all())
+            obj.files.add(*original_exercise.files.all())
+
             self.message_user(request, f"Exercise {obj.title} created successfully.")
-            return redirect(reverse('admin:exercise_exercise_change', args=[obj.id]))
+            return redirect(reverse("admin:exercise_exercise_change", args=[obj.id]))
         return super().response_change(request, obj)
 
 
