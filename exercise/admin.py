b8cba832a4e426325ea6089b2f6fc8bb21f2b6a6
Asghar Sir <asgharsir@gmail.com>
2019-10-21 14:24:34 +0100

    307:added filter and search to ExerciseEmailProperties

--- exercise/admin.py
+++ exercise/admin.py
@@ -49,6 +49,8 @@ class ExerciseAdmin(admin.ModelAdmin):
 @admin.register(ExerciseEmailProperties)
 class ExerciseEmailPropertiesAdmin(admin.ModelAdmin):
     list_display = ("exercise", "email", "reveal_time")
+    list_filter = ("exercise",)
+    search_fields = ["email__subject"]
 
 
 @admin.register(ExerciseWebPageReleaseCode)
