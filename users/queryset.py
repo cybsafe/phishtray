5953bb8a4af76cde51a6495c2470023b99f0ba6c
Asghar Sir <asgharsir@gmail.com>
2019-10-22 15:11:45 +0100

    added handling and tests for UserQuerySet

--- users/queryset.py
+++ users/queryset.py
@@ -1,9 +1,12 @@
-from django.db.models import QuerySet, Q
+from django.db.models import QuerySet
 
 
 class UserQuerySet(QuerySet):
     def filter_by_user(self, user):
         from participant.models import Organization
+
+        if not user or not isinstance(user, self.model):
+            return self.none()
 
         if not user.is_superuser:
             return self.filter(
---
+++ users/tests/test_querysets.py
@@ -0,0 +1,40 @@
+from rest_framework.test import APITestCase
+from users.models import User
+from participant.factories import OrganizationFactory
+from users.factories import UserFactory
+
+
+class UserQuerysetTests(APITestCase):
+    def setUp(self):
+        self.organization = OrganizationFactory()
+        self.other_organization = OrganizationFactory()
+        self.user1 = UserFactory(organization=self.organization)
+        self.user2 = UserFactory(organization=self.organization)
+        self.superuser = UserFactory(organization=self.organization, is_superuser=True)
+        self.user_of_other_organization = UserFactory(
+            organization=self.other_organization
+        )
+
+    def test_a_user(self):
+        users = User.objects.filter_by_user(user=self.user1)
+        user_organizations = users.values_list("organization__name", flat=True)
+
+        self.assertEqual(3, users.count())
+        self.assertTrue(self.organization.name in user_organizations)
+        self.assertFalse(self.other_organization.name in user_organizations)
+
+    def test_a_superuser(self):
+        users = User.objects.filter_by_user(user=self.superuser)
+        user_organizations = users.values_list("organization__name", flat=True)
+
+        self.assertEqual(4, users.count())
+        self.assertTrue(self.organization.name in user_organizations)
+        self.assertTrue(self.other_organization.name in user_organizations)
+
+    def test_when_user_is_none(self):
+        user = User.objects.filter_by_user(user=None)
+        self.assertFalse(user.exists())
+
+    def test_when_user_is_not_user_instance(self):
+        user = User.objects.filter_by_user(user=None)
+        self.assertFalse(user.exists())
