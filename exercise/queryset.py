5d2c76d1e3c378b5f558a46073d70724e08fbbe0
Marcelo Pacobahyba <marcelo@cybsafe.com>
2019-10-22 17:47:01 +0100

    Excluding soft deleted records from queryset

--- exercise/queryset.py
+++ exercise/queryset.py
@@ -3,11 +3,13 @@ from django.db.models import QuerySet
 
 class ExerciseQuerySet(QuerySet):
     def filter_by_user(self, user):
-        public_exercises = self.filter(organisation=None)
+        public_exercises = self.filter(organisation=None, deleted_at=None)
         if not user.is_superuser:
             if user.organization is None:
                 return public_exercises
             else:
-                private_exercises = self.filter(organisation=user.organization)
+                private_exercises = self.filter(
+                    organisation=user.organization, deleted_at=None
+                )
                 return private_exercises | public_exercises
         return self
