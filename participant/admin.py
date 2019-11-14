67ac728f4c9050020be42bbcca1418dea795cb3f
Marcelo Pacobahyba <marcelo@cybsafe.com>
2019-11-14 19:02:15 +0000

    Organizing methods for CSV generator

--- participant/admin.py
+++ participant/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from .models import Participant, Organization, ActionLog
+from .models import Participant, Organization
 from .filters import TrialVersionListFilter, ExerciseListFilter
+from .helpers import ExportCSVMixinHelpers
 from django.http import HttpResponse
-from django.db.models import F, Q, Value, CharField, OuterRef, Subquery, Count
-from django.db.models.functions import Coalesce
-from exercise.models import Exercise
+
 import csv
 
 
@@ -15,117 +14,17 @@ class ExportCsvMixin:
 
         exercise_ids = queryset.values_list("exercise_id")
 
-        exercise_qs = Exercise.objects.filter(id__in=exercise_ids)
-
-        exercise_participants_qs = (
-            Participant.objects.filter(exercise_id=OuterRef("pk"))
-            .values("exercise_id")
-            .annotate(participant_count=Count("exercise_id"))
-        )
-
-        participant_actions_qs = (
-            ActionLog.objects.filter(action__participant__exercise_id=OuterRef("pk"))
-            .values("action__participant__exercise_id")
-            .annotate(
-                dcount=Count("action__participant__exercise_id"),
-                emails_opened=Count(
-                    "id", filter=Q(name="action_type", value="email_opened")
-                ),
-                emails_reported=Count(
-                    "id", filter=Q(name="action_type", value="email_reported")
-                ),
-                emails_deleted=Count(
-                    "id", filter=Q(name="action_type", value="email_deleted")
-                ),
-                emails_linked_click=Count(
-                    "id", filter=Q(name="action_type", value="email_link_clicked")
-                ),
-                clicked_training_link=Count(
-                    "id", filter=Q(name="action_type", value="training_link_clicked")
-                ),
-                email_opened_attachment=Count(
-                    "id",
-                    filter=Q(name="action_type", value="email_attachment_download"),
-                ),
-                webpage_clicked=Count(
-                    "id", filter=Q(name="action_type", value="webpage_click")
-                ),
-                emails_replied=Count(
-                    "id", filter=Q(name="action_type", value="email_replied")
-                ),
-            )
-        )
-
-        field_names = [
-            "exercise_title",
-            "exercise_trial_version",
-            "emails_opened",
-            "phishing_emails_opened",
-            "pos_reported",
-            "pos_deleted",
-            "neg_clicked_link",
-            "neg_entered_detail",
-            "neg_replied_to_phishing_email",
-            "neg_opened_attachment",
-            "participant_count",
-            "training_link_clicked",
-        ]
-
-        queryset = exercise_qs.annotate(
-            exercise_title=F("title"),
-            exercise_trial_version=F("trial_version"),
-            emails_opened=Coalesce(
-                Subquery(
-                    participant_actions_qs.values("emails_opened")[:1],
-                    output_field=CharField(),
-                ),
-                0,
-            ),
-            phishing_emails_opened=Value(0, output_field=CharField()),
-            pos_reported=Subquery(
-                participant_actions_qs.values("emails_reported")[:1],
-                output_field=CharField(),
-            ),
-            pos_deleted=Subquery(
-                participant_actions_qs.values("emails_deleted")[:1],
-                output_field=CharField(),
-            ),
-            neg_clicked_link=Subquery(
-                participant_actions_qs.values("emails_linked_click")[:1],
-                output_field=CharField(),
-            ),
-            neg_entered_detail=Subquery(
-                participant_actions_qs.values("webpage_clicked")[:1],
-                output_field=CharField(),
-            ),
-            neg_replied_to_phishing_email=Subquery(
-                participant_actions_qs.values("emails_replied")[:1],
-                output_field=CharField(),
-            ),
-            neg_opened_attachment=Subquery(
-                participant_actions_qs.values("email_opened_attachment")[:1],
-                output_field=CharField(),
-            ),
-            participant_count=Subquery(
-                exercise_participants_qs.values("participant_count")[:1],
-                output_field=CharField(),
-            ),
-            training_link_clicked=Coalesce(
-                Subquery(
-                    participant_actions_qs.values("clicked_training_link")[:1],
-                    output_field=CharField(),
-                ),
-                Value("N.A."),
-            ),
+        csv_columns, csv_rows = ExportCSVMixinHelpers().get_participant_stats_csv_data(
+            exercise_ids
         )
 
         response = HttpResponse(content_type="text/csv")
         response["Content-Disposition"] = "attachment; filename={}.csv".format(meta)
         writer = csv.writer(response)
 
-        writer.writerow(field_names)
-        for obj in queryset:
-            writer.writerow([getattr(obj, field) for field in field_names])
+        writer.writerow(csv_columns)
+        for obj in csv_rows:
+            writer.writerow([getattr(obj, field) for field in csv_columns])
 
         return response
 
---
+++ participant/helpers.py
@@ -0,0 +1,96 @@
+from django.db.models import F, Q, Value, CharField, OuterRef, Subquery, Count
+from django.db.models.functions import Coalesce
+
+from exercise.models import Exercise
+from .models import Participant, ActionLog
+
+
+class ExportCSVMixinHelpers:
+    def get_participant_stats_csv_data(self, exercise_ids):
+
+        exercise_qs = Exercise.objects.filter(id__in=exercise_ids)
+
+        exercise_participants_qs = (
+            Participant.objects.filter(exercise_id=OuterRef("pk"))
+            .values("exercise_id")
+            .annotate(participant_count=Count("exercise_id"))
+        )
+
+        participant_actions_qs = (
+            ActionLog.objects.filter(action__participant__exercise_id=OuterRef("pk"))
+            .values("action__participant__exercise_id")
+            .annotate(
+                dcount=Count("action__participant__exercise_id"),
+                emails_opened=self.get_action_count("email_opened"),
+                emails_reported=self.get_action_count("email_reported"),
+                emails_deleted=self.get_action_count("email_deleted"),
+                emails_linked_click=self.get_action_count("email_link_clicked"),
+                clicked_training_link=self.get_action_count("training_link_clicked"),
+                email_opened_attachment=self.get_action_count(
+                    "email_attachment_download"
+                ),
+                webpage_clicked=self.get_action_count("webpage_click"),
+                emails_replied=self.get_action_count("email_replied"),
+            )
+        )
+
+        csv_columns = [
+            "exercise_title",
+            "exercise_trial_version",
+            "emails_opened",
+            "phishing_emails_opened",
+            "pos_reported",
+            "pos_deleted",
+            "neg_clicked_link",
+            "neg_entered_detail",
+            "neg_replied_to_phishing_email",
+            "neg_opened_attachment",
+            "participant_count",
+            "training_link_clicked",
+        ]
+
+        csv_rows = exercise_qs.annotate(
+            exercise_title=F("title"),
+            exercise_trial_version=F("trial_version"),
+            emails_opened=Coalesce(
+                self.get_subquery_value(participant_actions_qs, "emails_opened"), 0
+            ),
+            phishing_emails_opened=Value(0, output_field=CharField()),
+            pos_reported=self.get_subquery_value(
+                participant_actions_qs, "emails_reported"
+            ),
+            pos_deleted=self.get_subquery_value(
+                participant_actions_qs, "emails_deleted"
+            ),
+            neg_clicked_link=self.get_subquery_value(
+                participant_actions_qs, "emails_linked_click"
+            ),
+            neg_entered_detail=self.get_subquery_value(
+                participant_actions_qs, "webpage_clicked"
+            ),
+            neg_replied_to_phishing_email=self.get_subquery_value(
+                participant_actions_qs, "emails_replied"
+            ),
+            neg_opened_attachment=self.get_subquery_value(
+                participant_actions_qs, "email_opened_attachment"
+            ),
+            participant_count=self.get_subquery_value(
+                exercise_participants_qs, "participant_count"
+            ),
+            training_link_clicked=Coalesce(
+                self.get_subquery_value(
+                    participant_actions_qs, "clicked_training_link"
+                ),
+                Value("N.A."),
+            ),
+        )
+
+        return csv_columns, csv_rows
+
+    @staticmethod
+    def get_subquery_value(queryset, value):
+        return Subquery(queryset.values(value)[:1], output_field=CharField())
+
+    @staticmethod
+    def get_action_count(action):
+        return Count("id", filter=Q(name="action_type", value=action))
