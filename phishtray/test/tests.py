ee4a9700c102d4f170ec78536b45cbacda050b8d
Feri Csepregi <feri@31.digital>
2019-11-15 13:21:40 +0000

    fixup! 384 - strat to delete items on .delete() and purge soft deleted records

--- phishtray/test/tests.py
+++ phishtray/test/tests.py
@@ -10,36 +10,25 @@ class PhishtrayBaseModelTest(TestCase):
         self.exercise_2 = ExerciseFactory()
 
     def test_soft_delete(self):
-        # Check that we have all Exercises
-        self.assertEqual(2, Exercise.objects.all().count())
+        # IMPORTANT soft delete is being deprecated, at this point it should work as delete
 
-        # Delete one then we should end up with 10 when accessing the DB with django
+        # Check that we have all Exercises
+        self.assertEqual(2, Exercise.objects.count())
+
         self.exercise_1.delete()
         self.assertFalse(Exercise.objects.filter(pk=self.exercise_1.id))
-        self.assertEqual(1, Exercise.objects.all().count())
+        self.assertEqual(1, Exercise.objects.count())
 
-        # However the record should still be present in the DB
+        # The record should be deleted from the DB as well
         raw_exercises = Exercise.objects.raw("SELECT * FROM exercise_exercise")
 
         all_exercises = [e for e in raw_exercises]
         deleted_exercises = [e for e in raw_exercises if e.deleted_at]
         undeleted_exercises = [e for e in raw_exercises if not e.deleted_at]
 
-        self.assertEqual(2, len(all_exercises))
-        self.assertEqual(1, len(deleted_exercises))
+        self.assertEqual(1, len(all_exercises))
+        self.assertEqual(0, len(deleted_exercises))
         self.assertEqual(1, len(undeleted_exercises))
-
-    def test_undo_soft_delete(self):
-        # Delete one then we should end up with 10 when accessing the DB with django
-        self.exercise_1.delete()
-        self.assertFalse(Exercise.objects.filter(pk=self.exercise_1.id))
-        self.assertEqual(1, Exercise.objects.all().count())
-
-        exercise = Exercise.all_objects.get(pk=self.exercise_1.id)
-        exercise.deleted_at = None
-        exercise.save()
-
-        self.assertEqual(2, Exercise.objects.all().count())
 
     def test_hard_delete(self):
         self.exercise_1.hard_delete()
