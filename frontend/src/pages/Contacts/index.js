395a2e4a41d8f671fc427f6532bf988ea2328c96
Danylo Santoro <danylo.santoro@gmail.com>
2019-11-15 10:37:24 +0000

    fix participant reducer

--- frontend/src/pages/Contacts/index.js
+++ frontend/src/pages/Contacts/index.js
@@ -49,7 +49,7 @@ function NoMatch() {
 }
 
 function Contacts({ match }) {
-  const participantId = useSelector(state => state.exercise.participantId);
+  const participantId = useSelector(state => state.exercise.participant);
   const startTime = useSelector(state => state.exercise.startTime);
 
   const logActionsHandler = params => {
