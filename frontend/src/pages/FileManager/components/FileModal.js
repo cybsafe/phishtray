ecd12769c7a74d2fb857672f5c2f6a7042217446
Ben Diggle <ben.diggle5@gmail.com>
2020-01-24 16:22:38 +0000

    make image modal larger and add scroll bar for overflow (#445)
    Make Modal bigger
    Add scroll permanently when needed to show user that they can scroll down

--- frontend/src/pages/FileManager/components/FileModal.js
+++ frontend/src/pages/FileManager/components/FileModal.js
@@ -6,6 +6,23 @@ const ModalContainer = styled('div')`
   & .bx--modal-footer {
     display: none;
   }
+  .bx--modal-container {
+    max-width: 80%;
+  }
+  .bx--modal-header {
+    margin: 0;
+  }
+  .bx--modal-content {
+    &::-webkit-scrollbar {
+      -webkit-appearance: none;
+      height: 11px;
+    }
+    &::-webkit-scrollbar-thumb {
+      border-radius: 8px;
+      border: 3px solid white;
+      background-color: rgba(0, 0, 0, 0.5);
+    }
+  }
 `;
 
 const FileImage = styled('img')({
